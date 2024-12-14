984cb2ade999f10f8962bf8575ec2380b4faf5ee
NatanNMendes <natan_nunes_mendes@outlook.com>
2024-12-14 12:39:17 -0300

    Validação de Login com Firestorage

--- app/src/main/java/com/example/bancodedados/LoginActivity.java
+++ app/src/main/java/com/example/bancodedados/LoginActivity.java
@@ -1,11 +1,15 @@
 package com.example.bancodedados;
 
 import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Handler;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
 import android.view.View;
@@ -15,8 +19,24 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Objects;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private TextView text_tela_cadastro;
+    private TextView edit_email, edit_senha;
+    private Button btn_entrar;
+    private ProgressBar progress_bar;
+    String[] mensagens = {"Preencha todos os campos", "Login realizado com sucesso", "Erro ao realizar login"};
 
     private boolean isPasswordVisible = false;
 
@@ -30,12 +50,71 @@ public class LoginActivity extends AppCompatActivity {
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
         window.setStatusBarColor(getResources().getColor(R.color.white));
 
+        EditText emailInput = findViewById(R.id.usernameInput);
         EditText passwordInput = findViewById(R.id.passwordInput);
         ImageButton togglePasswordButton = findViewById(R.id.togglePasswordButton);
         LinearLayout signUpButton = findViewById(R.id.sign_up_button);
+        Button loginButton = findViewById(R.id.loginButton);
 
-
+        loginButton.setOnClickListener(v -> validarLogin(loginButton));
+        togglePasswordButton.setOnClickListener(v -> togglePasswordVisibility(passwordInput,togglePasswordButton));
         signUpButton.setOnClickListener(v -> goToCreateAccount());
+    }
+
+    public void validarLogin(View v) {
+
+        String email = edit_email.getText().toString();
+        String senha = edit_senha.getText().toString();
+
+        if (email.isEmpty() || senha.isEmpty()) {
+            Toast toast = Toast.makeText(LoginActivity.this, mensagens[0], Toast.LENGTH_SHORT);
+            Objects.requireNonNull(toast.getView()).setBackgroundTintList(ColorStateList.valueOf(Color.WHITE));
+            TextView text = toast.getView().findViewById(android.R.id.message);
+            text.setTextColor(Color.BLACK);
+            toast.show();
+        } else {
+            AutenticarUsuario(v);
+        }
+    }
+
+    private void AutenticarUsuario(View view) {
+
+        String email = edit_email.getText().toString();
+        String senha = edit_senha.getText().toString();
+
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(email, senha).addOnCompleteListener((new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    progress_bar.setVisibility(View.VISIBLE);
+
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            IrParaPerfilActivity();
+                        }
+                    }, 2000);
+                } else {
+                    String erro;
+
+                    try {
+                        throw task.getException();
+                    } catch (Exception e) {
+                        erro = "Erro ao realizar login";
+                    }
+                    Toast toast = Toast.makeText(LoginActivity.this, erro, Toast.LENGTH_SHORT);
+                    Objects.requireNonNull(toast.getView()).setBackgroundTintList(ColorStateList.valueOf(Color.WHITE));
+                    TextView text = toast.getView().findViewById(android.R.id.message);
+                    text.setTextColor(Color.BLACK);
+                    toast.show();
+                }
+            }
+        }));
+    }
+
+    private void IrParaPerfilActivity() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 
     public void goToBackScreen(View view) {
--- app/src/main/java/com/example/bancodedados/MainActivity.java
+++ app/src/main/java/com/example/bancodedados/MainActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,7 +14,6 @@ import androidx.core.content.ContextCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import com.bumptech.glide.Glide;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -36,12 +34,6 @@ public class MainActivity extends AppCompatActivity {
 
         auth = FirebaseAuth.getInstance();
         FirebaseUser currentUser = auth.getCurrentUser();
-
-        ImageView gifImageView = findViewById(R.id.gifImageView);
-        Glide.with(this)
-                .asGif()
-                .load(R.raw.machamp)
-                .into(gifImageView);
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             v.setPadding(insets.getInsets(WindowInsetsCompat.Type.systemBars()).left,
